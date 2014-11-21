4e92b7b8732e384a39a888d2a31ed1f0fbb02c52
Danica Christine Corpuz <dccorpuz1@yahoo.com>
2014-11-21 23:24:29 +0800

    logbean

---
+++ WEW/src/java/Beans/LogBean.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Beans;
+
+import java.sql.Timestamp;
+
+/**
+ *
+ * @author Danica
+ */
+public class LogBean {
+    private int logID;
+    private int log_accountID;
+    private String activity;
+    private Timestamp time;
+
+    public int getLogID() {
+        return logID;
+    }
+
+    public void setLogID(int logID) {
+        this.logID = logID;
+    }
+
+    public int getLog_accountID() {
+        return log_accountID;
+    }
+
+    public void setLog_accountID(int log_accountID) {
+        this.log_accountID = log_accountID;
+    }
+
+    public String getActivity() {
+        return activity;
+    }
+
+    public void setActivity(String activity) {
+        this.activity = activity;
+    }
+
+    public Timestamp getTime() {
+        return time;
+    }
+
+    public void setTime(Timestamp time) {
+        this.time = time;
+    }
+}
