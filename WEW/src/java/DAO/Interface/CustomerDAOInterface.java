71bcd1ff1fbdffee34c699fe23c4e64fbc31784e
giodeealbayda <giodeelyn_albayda@dlsu.edu.ph>
2014-11-22 11:26:40 +0800

    AudioCDDAOInterface and Implementation

--- WEW/src/java/DAO/Interface/CustomerDAOInterface.java
+++ WEW/src/java/DAO/Interface/CustomerDAOInterface.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package DAO.Interface;
 
 import Beans.AudioCDBean;
@@ -12,24 +11,25 @@ import Beans.ProductBean;
 import java.util.ArrayList;
 
 public interface CustomerDAOInterface {
-       public boolean addCustomer(CustomerBean customerBean);
-       //public boolean removeCustomer(int customerID);
-       public CustomerBean getCustomerById(int customerID);
-       public ArrayList<CustomerBean> getAllCustomers();
-       
-       public boolean addToCart (ProductBean product);
-       public boolean removeFromCart (ProductBean product);
-       public ArrayList<ProductBean> viewCart();
-       public boolean purchase(int productID);
-       public ProductBean viewProduct (int ProductID);
-       public ProductBean searchProduct (int ProductID);
-       
-       public AudioCDBean getAudioCDByID(int id);
-       public AudioCDBean getAudioCDByProductID(int productID);
-       public ArrayList<AudioCDBean> getAudioCDByArtist(String artist);
-       public ArrayList<AudioCDBean> getAudioCDByRecordCompany(String recordCompany);
-       
-       
-       
- 
+
+    public boolean addCustomer(CustomerBean customerBean);
+
+    //public boolean removeCustomer(int customerID);
+
+    public CustomerBean getCustomerById(int customerID);
+
+    public ArrayList<CustomerBean> getAllCustomers();
+
+    public boolean addToCart(ProductBean product);
+
+    public boolean removeFromCart(ProductBean product);
+
+    public ArrayList<ProductBean> viewCart();
+
+    public boolean purchase(int productID);
+
+    public ProductBean viewProduct(int ProductID);
+
+    public ProductBean searchProduct(int ProductID);
+
 }
